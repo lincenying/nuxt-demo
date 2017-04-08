5432ecec69ad8b274fd035379fe6cc76fdebaf9f
lincenying <lincenying@qq.com>
2017-04-08 16:47:02 +0800

    更新

--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,17 @@
     <menu-s></menu-s>
     <p>{{ userAgent }}</p>
     <p>Vuex: {{ counter }} <a href="javascript:;" @click="increment">加1个</a></p>
+    <el-table :data="tableData" style="width: 100%">
+        <el-table-column prop="date" label="日期" width="180"> </el-table-column>
+        <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+        <el-table-column prop="address" label="地址"> </el-table-column>
+    </el-table>
 </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import { Table, TableColumn } from 'element-ui'
 import menuS from '~components/menu.vue'
 export default {
     name: 'index',
@@ -21,11 +27,28 @@ export default {
     computed: mapState([
         'counter'
     ]),
-    data({ req }, callback) {
+    asyncData({ req }, callback) {
         setTimeout(function() {
             // callback(err, data)
             callback(null, {
-                userAgent: req ? req.headers['user-agent'] : navigator.userAgent
+                userAgent: req ? req.headers['user-agent'] : navigator.userAgent,
+                tableData: [{
+                    date: '2016-05-02',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                }, {
+                    date: '2016-05-04',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1517 弄'
+                }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1519 弄'
+                }, {
+                    date: '2016-05-03',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1516 弄'
+                }]
             })
         }, 100)
     },
@@ -35,7 +58,9 @@ export default {
         }
     },
     components: {
-        menuS
+        menuS,
+        elTable: Table,
+        elTableColumn: TableColumn,
     },
     methods: {
         increment() {
